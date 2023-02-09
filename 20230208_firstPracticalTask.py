27ceb1fd7bda871159f6f31ad3a7dd7d61572a5e
OleksiyRudeyko <123978040+OleksiyRudeyko@users.noreply.github.com>
2023-02-09 09:46:06 +0200

    First practical task
    Done homework from the first lesson of Python Fundamentals course at SoftServe.

---
+++ 20230208_firstPracticalTask.py
@@ -0,0 +1,5 @@
+name = input("What's your name?\n")
+age = input("How old are you?\n")
+location = input("Where are you from?\n")
+
+print(f"Hello, {name}! Your age is {age}. You're from {location}.")
